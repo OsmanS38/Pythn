a8ea053e1b86ab07f47b0567933091e80c29ab3b
Osman Seker <seos@itadh.mgeg.de>
2022-01-14 22:23:25 +0100

    einkauft.py wurde hinzugefügt

--- einkauft.py
+++ einkauft.py
@@ -9,11 +9,12 @@ a ={'name':[], 'menge':[], 'preis':[]}
 #b =list(a.werke())
 
 while True:
-    neu_dinge = input('Geben Sie ein Obst ,Gemueze:') 
+    obst = input('Geben Sie ein Obst ,Gemueze:') 
      #(wenn hat in list:q)')
-    if neu_dinge not in list:
-        list += [neu_dinge]
-        print(list)
+    if obst,gemueze != 'q'
+        list.append(obst)
+        list.append(gemueze)
     else:
-        print('Diese sache haben Sie doch!')
-
+       break
+print(list)
+print(len(list))
